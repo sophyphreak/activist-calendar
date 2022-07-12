aa1a3567ca59fc38ed63f31f4820747c09336a56
Franck Boucher <boucher.fk@gmail.com>
2022-07-12 21:43:52 +0200

    Smaller size for past events

--- src/components/EventEntry.tsx
+++ src/components/EventEntry.tsx
@@ -10,19 +10,27 @@ export type EventType = {
 export interface EventEntryProps {
   key?: any;
   event: EventType;
+  small?: boolean;
 }
-export default function EventEntry({ event }: EventEntryProps) {
+export default function EventEntry({ event, small = false }: EventEntryProps) {
+  const iconSize = small ? "h-6 w-6" : "h-8 w-8";
+  const margin = small ? "mx-16" : "";
+  const padding = small ? "p-3" : "p-6";
   return (
-    <div className="bg-white border-solid border border-gray-200 drop-shadow-md p-6 flex gap-10 items-center justify-between my-4">
+    <div
+      className={`bg-white border-solid border border-gray-200 drop-shadow-md flex gap-10 items-center justify-between my-4 ${padding} ${margin}`}
+    >
       <span className="flex gap-8 items-center ">
-        <span className="text-3xl font-bold">{event.date}</span>
-        <span className="text-2xl">{event.title}</span>
+        <span className={`${small ? "text-xl" : "text-3xl"} font-bold`}>
+          {event.date}
+        </span>
+        <span className={small ? "text-lg" : "text-2xl"}>{event.title}</span>
       </span>
 
       <span className="flex gap-4 items-center">
-        <Annotation className="h-8 w-8 text-gray-900" />
-        <LocationMarker className="h-8 w-8 text-red-900" />
-        <Pencil className="h-8 w-8 text-blue-900" />
+        <Annotation className={`${iconSize} text-gray-900`} />
+        <LocationMarker className={`${iconSize} text-red-900`} />
+        <Pencil className={`${iconSize} text-blue-900`} />
       </span>
     </div>
   );
--- src/components/RecentEventsSection.tsx
+++ src/components/RecentEventsSection.tsx
@@ -8,9 +8,11 @@ export default function RecentEventsSection({
 }: RecentEventsSectionProps) {
   return (
     <section>
-      <h2 className="text-center text-4xl font-bold mb-8">Recent events</h2>
+      <h2 className="text-center text-3xl font-bold mb-8">Recent events</h2>
       {events.length > 0 ? (
-        events.map((event) => <EventEntry key={event.title} event={event} />)
+        events.map((event) => (
+          <EventEntry key={event.title} event={event} small />
+        ))
       ) : (
         <p className="text-center italic">No past events.</p>
       )}
